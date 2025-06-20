d1deb326e3adc21bc55b0000ed676376295c2d7f
Cameron Woodbury <cam@hobu.co>
2025-06-20 13:36:04 -0500

    Add social links as icons
    Added react-icons to get access to Mastodon, Github, and Linkedin brand
    icons, in order to link my socials.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import { Box, Center, Text } from '@mantine/core'
+import { ActionIcon, Box, Center, Group, Stack, Text } from '@mantine/core'
+import { FaGithub, FaLinkedin, FaMastodon } from 'react-icons/fa'
 
 function App() {
   return (
     <Center h='100vh'>
-      <Box>
-        <Text>My name is Cameron</Text>
-        <Text>Welcome to my website</Text>
-      </Box>
+      <Stack>
+        <Box>
+          <Text>My name is Cameron</Text>
+          <Text>Welcome to my website</Text>
+        </Box>
+        <Group grow>
+          <ActionIcon
+            size='lg'
+            variant='light'
+            color='hobu.6'
+            component='a'
+            href='https://fosstodon.org/@themrcam'
+            target='_blank'
+          >
+            <FaMastodon size={20} />
+          </ActionIcon>
+          <ActionIcon
+            size='lg'
+            variant='light'
+            color='hobu.6'
+            component='a'
+            href='https://github.com/themrcam'
+            target='_blank'
+          >
+            <FaGithub size={20} />
+          </ActionIcon>
+          <ActionIcon
+            size='lg'
+            variant='light'
+            color='hobu.6'
+            component='a'
+            href='https://linkedin.com/in/themrcam/'
+            target='_blank'
+          >
+            <FaLinkedin size={20} />
+          </ActionIcon>
+        </Group>
+      </Stack>
     </Center>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { createTheme, MantineProvider } from '@mantine/core'
+import App from './app'
+import { createTheme, type MantineColorsTuple, MantineProvider } from '@mantine/core'
 
 import '@mantine/core/styles.css'
 
-const theme = createTheme({})
+const hobu: MantineColorsTuple = [
+  '#e7f7ff',
+  '#d7e9f8',
+  '#b2d0e9',
+  '#89b5da',
+  '#679fce',
+  '#5190c7',
+  '#4188c4',
+  '#3377ae',
+  '#27699d',
+  '#115b8c',
+]
+
+const theme = createTheme({
+  colors: { hobu },
+  primaryColor: 'hobu',
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
